d9a4a7aaac3420960b058f1e520429e20d08d9a6
Iparshutin <iparshytin@gmail.com>
2019-10-19 16:52:31 +0300

    Setting up and checking project dependencies
    Creating a "hello world" on vaadin and spring

---
+++ src/main/java/com/parshutin/storage/view/MainView.java
@@ -0,0 +1,13 @@
+package com.parshutin.storage.view;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+    public MainView() {
+        add(new Button("Test",e -> Notification.show("click")));
+    }
+}
