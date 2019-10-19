7adf256936534baf3e505d80fe32f9ceeef970ec
Iparshutin <iparshytin@gmail.com>
2019-10-19 17:41:05 +0300

    Display data from the database on the MainView

--- src/main/java/com/parshutin/storage/view/MainView.java
+++ src/main/java/com/parshutin/storage/view/MainView.java
@@ -1,13 +1,28 @@
 package com.parshutin.storage.view;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.parshutin.storage.datasets.Product;
+import com.parshutin.storage.repositories.ProductRepository;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainView extends VerticalLayout {
-    public MainView() {
-        add(new Button("Test",e -> Notification.show("click")));
+    private ProductRepository productRepository;
+
+    private Grid<Product> grid = new Grid<>(Product.class);
+
+    public MainView(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+
+        grid.setColumns("title","type","count","deliveryDate");
+
+        updateList();
+
+        add(grid);
+    }
+
+    public void updateList(){
+        grid.setItems(productRepository.findAll());
     }
 }
