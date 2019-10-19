f1c4c184a02937293e5d37d7ed9c2741d2a89791
Iparshutin <iparshytin@gmail.com>
2019-10-19 17:53:22 +0300

    Added a filter by title

--- src/main/java/com/parshutin/storage/repositories/ProductRepository.java
+++ src/main/java/com/parshutin/storage/repositories/ProductRepository.java
@@ -3,6 +3,8 @@ package com.parshutin.storage.repositories;
 import com.parshutin.storage.datasets.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product,Integer> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    List<Product> findByTitleStartsWithIgnoreCase(String name);
 }
--- src/main/java/com/parshutin/storage/view/MainView.java
+++ src/main/java/com/parshutin/storage/view/MainView.java
@@ -4,25 +4,32 @@ import com.parshutin.storage.datasets.Product;
 import com.parshutin.storage.repositories.ProductRepository;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainView extends VerticalLayout {
     private ProductRepository productRepository;
 
+    private TextField filter = new TextField();
     private Grid<Product> grid = new Grid<>(Product.class);
 
     public MainView(ProductRepository productRepository) {
         this.productRepository = productRepository;
 
+        filter.setPlaceholder("Filter by title");
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> updateList());
+
         grid.setColumns("title","type","count","deliveryDate");
 
         updateList();
 
-        add(grid);
+        add(filter,grid);
     }
 
     public void updateList(){
-        grid.setItems(productRepository.findAll());
+        grid.setItems(productRepository.findByTitleStartsWithIgnoreCase(filter.getValue()));
     }
 }
